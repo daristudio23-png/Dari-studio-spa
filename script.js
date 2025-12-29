a32e567ba996e6693267fabb28f6795579193b1f
daristudio23-png <daristudio23@gmail.com>
2025-12-29 00:13:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Script base listo para Ads, Pixel o Analytics
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Dari Studio Spa - sitio cargado correctamente");
+});
+
+/*
+Aquí puedes agregar luego:
+- Meta Pixel
+- Google Analytics / Tag Manager
+- Eventos de clic WhatsApp o AgendaPro
+*/
