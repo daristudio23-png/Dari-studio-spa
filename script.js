9752471874462975e19a136f747b5702895866a3
daristudio23-png <daristudio23@gmail.com>
2025-12-30 03:10:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Dari Studio Spa - sitio cargado correctamente");
+  console.log("Dari Studio Spa cargado correctamente");
 
   document.querySelectorAll(".whatsapp-link").forEach(btn => {
     btn.addEventListener("click", () => {
       console.log("Click WhatsApp");
-      // fbq('trackCustom', 'WhatsAppClick');
     });
   });
 
   document.querySelectorAll(".pagar-link").forEach(btn => {
     btn.addEventListener("click", () => {
       console.log("Click Mercado Pago");
-      // fbq('trackCustom', 'PagoKeratina');
     });
   });
 });
