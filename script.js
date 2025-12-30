058a462e53a1369fb40246f15e9a7c3bd274b0e3
daristudio23-png <daristudio23@gmail.com>
2025-12-30 01:42:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-// Script base listo para Ads, Pixel o Analytics
-
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Dari Studio Spa - sitio cargado correctamente");
-});
 
-/*
-Aquí puedes agregar luego:
-- Meta Pixel
-- Google Analytics / Tag Manager
-- Eventos de clic WhatsApp o AgendaPro
-*/
+  document.querySelectorAll(".whatsapp-link").forEach(btn => {
+    btn.addEventListener("click", () => {
+      console.log("Click WhatsApp");
+      // fbq('trackCustom', 'WhatsAppClick');
+    });
+  });
+
+  document.querySelectorAll(".pagar-link").forEach(btn => {
+    btn.addEventListener("click", () => {
+      console.log("Click Mercado Pago");
+      // fbq('trackCustom', 'PagoKeratina');
+    });
+  });
+});
