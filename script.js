b551e078e20338d7684a844225f02cde257531c0
daristudio23-png <daristudio23@gmail.com>
2026-01-02 04:16:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const items = document.querySelectorAll(".reveal");
 
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("in-view");
-          observer.unobserve(entry.target);
-        }
-      });
-    },
-    { threshold: 0.15 }
-  );
-
-  items.forEach((el) => observer.observe(el));
-});
